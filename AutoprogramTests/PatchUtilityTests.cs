f21da172d55491af34820fbd9dd4c0dbe948f4be
Lars Liden <larsliden@gmail.com>
2023-04-21 15:28:17 -0700

    Manual patching
    GPT4 results are noisy (doesn't get unidiff number right) so do patching manually

--- AutoprogramTests/PatchUtilityTests.cs
+++ AutoprogramTests/PatchUtilityTests.cs
@@ -16,7 +16,7 @@ namespace Autoprogram.Tests
                                   "}\n";
 
             string unidiff = "@@ -1,5 +1,5 @@\n" +
-                             " public class HelloWorld {\n" +
+                             "  public class HelloWorld {\n" +  // Tests extra spaces
                              "     public static void main(String[] args) {\n" +
                              "-        System.out.println(\"Hello, world!\");\n" +
                              "+        System.out.println(\"Hello, Earth!\");\n" +
@@ -29,11 +29,35 @@ namespace Autoprogram.Tests
                                          "    }\n" +
                                          "}\n";
 
+            var diffs = new List<string> { unidiff };
             // Act
-            string updatedCode = PatchUtility.ApplyPatch(originalCode, unidiff);
+            string updatedCode = PatchUtility.ApplyDiffs(originalCode, diffs);
 
             // Assert
             Assert.Equal(TestUtil.RemoveLF(expectedUpdatedCode), TestUtil.RemoveLF(updatedCode), ignoreLineEndingDifferences: true, ignoreWhiteSpaceDifferences: true);
+        }
+
+        [Fact]
+        public void ApplyPatch_ReturnsCorrectLines()
+        {
+            var codeLines = new List<string> { "A", "B", "C", "D", "E", "F", "G" };
+            var patch = new PatchUtility.Patch();
+            patch.Add(new PatchUtility.PatchLine("B", PatchUtility.PatchType.KEEP));
+            patch.Add(new PatchUtility.PatchLine("C", PatchUtility.PatchType.DELETE));
+            patch.Add(new PatchUtility.PatchLine("D", PatchUtility.PatchType.DELETE));
+            patch.Add(new PatchUtility.PatchLine("c", PatchUtility.PatchType.ADD));
+            patch.Add(new PatchUtility.PatchLine("d", PatchUtility.PatchType.ADD));
+            patch.Add(new PatchUtility.PatchLine("E", PatchUtility.PatchType.KEEP));
+            patch.Add(new PatchUtility.PatchLine("F", PatchUtility.PatchType.DELETE));
+            patch.Add(new PatchUtility.PatchLine("f", PatchUtility.PatchType.ADD));
+
+            var expectedCodeLines = new List<string> { "A", "B", "c", "d", "E", "f", "G" };
+
+            // Act
+            var updatedCodeLines = PatchUtility.ApplyPatchToLines(codeLines, patch);
+
+            // Assert
+            Assert.Equal(expectedCodeLines, updatedCodeLines);
         }
     }
 }
